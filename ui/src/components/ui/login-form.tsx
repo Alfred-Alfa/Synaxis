d270c01aa3d3336c5f19ba5e92d18dd1d5994bea
Sangeet Shaji <abdur@Sangeet-Macbook.local>
2026-01-06 18:23:38 +0530

    feat: replace tech icons with three company logos

--- ui/src/components/ui/login-form.tsx
+++ ui/src/components/ui/login-form.tsx
@@ -34,157 +34,49 @@ const iconsArray: OrbitIcon[] = [
             <img
                 width={80}
                 height={80}
-                src='/assets/logo.png'
-                alt='Company Logo'
+                src='/assets/logo-elitecraft.png'
+                alt='Elitecraft Logo'
                 className='rounded-full'
             />
         ),
         className: 'size-[80px] border-none bg-transparent',
         duration: 20,
         delay: 0,
-        radius: 150,
+        radius: 180,
         path: false,
         reverse: false,
     },
     {
         component: () => (
             <img
-                width={100}
-                height={100}
-                src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg'
-                alt='HTML5'
+                width={60}
+                height={60}
+                src='/assets/logo-cursor.png'
+                alt='Cursor Logo'
+                className='rounded-full'
             />
         ),
-        className: 'size-[30px] border-none bg-transparent',
+        className: 'size-[60px] border-none bg-transparent',
         duration: 20,
-        delay: 20,
-        radius: 100,
+        delay: 7,
+        radius: 180,
         path: false,
         reverse: false,
     },
     {
         component: () => (
             <img
-                width={100}
-                height={100}
-                src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg'
-                alt='CSS3'
+                width={60}
+                height={60}
+                src='/assets/logo-runner.png'
+                alt='Runner Logo'
+                className='rounded-full'
             />
         ),
-        className: 'size-[30px] border-none bg-transparent',
+        className: 'size-[60px] border-none bg-transparent',
         duration: 20,
-        delay: 10,
-        radius: 100,
-        path: false,
-        reverse: false,
-    },
-    {
-        component: () => (
-            <img
-                width={100}
-                height={100}
-                src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg'
-                alt='TypeScript'
-            />
-        ),
-        className: 'size-[50px] border-none bg-transparent',
-        radius: 210,
-        duration: 20,
-        path: false,
-        reverse: false,
-    },
-    {
-        component: () => (
-            <img
-                width={100}
-                height={100}
-                src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg'
-                alt='JavaScript'
-            />
-        ),
-        className: 'size-[50px] border-none bg-transparent',
-        radius: 210,
-        duration: 20,
-        delay: 20,
-        path: false,
-        reverse: false,
-    },
-    {
-        component: () => (
-            <img
-                width={100}
-                height={100}
-                src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg'
-                alt='TailwindCSS'
-            />
-        ),
-        className: 'size-[30px] border-none bg-transparent',
-        duration: 20,
-        delay: 20,
-        radius: 150,
-        path: false,
-        reverse: true,
-    },
-    {
-        component: () => (
-            <img
-                width={100}
-                height={100}
-                src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg'
-                alt='Nextjs'
-            />
-        ),
-        className: 'size-[30px] border-none bg-transparent',
-        duration: 20,
-        delay: 10,
-        radius: 150,
-        path: false,
-        reverse: true,
-    },
-    {
-        component: () => (
-            <img
-                width={100}
-                height={100}
-                src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg'
-                alt='React'
-            />
-        ),
-        className: 'size-[50px] border-none bg-transparent',
-        radius: 270,
-        duration: 20,
-        path: false,
-        reverse: true,
-    },
-    {
-        component: () => (
-            <img
-                width={100}
-                height={100}
-                src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg'
-                alt='Figma'
-            />
-        ),
-        className: 'size-[50px] border-none bg-transparent',
-        radius: 270,
-        duration: 20,
-        delay: 60,
-        path: false,
-        reverse: true,
-    },
-    {
-        component: () => (
-            <img
-                width={100}
-                height={100}
-                src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg'
-                alt='Git'
-            />
-        ),
-        className: 'size-[50px] border-none bg-transparent',
-        radius: 320,
-        duration: 20,
-        delay: 20,
+        delay: 14,
+        radius: 180,
         path: false,
         reverse: false,
     },
